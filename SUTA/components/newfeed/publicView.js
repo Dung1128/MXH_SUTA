8bab2216b135f778d10383e60fb019c409c70fe8
Dung1128 <trandung1128@gmail.com>
2017-04-29 14:22:32 +0700

    fix newfeed

--- SUTA/components/newfeed/publicView.js
+++ SUTA/components/newfeed/publicView.js
@@ -54,7 +54,7 @@ export default class Public extends Component{
     this.setState({
         spinnerVisible: true,
       });
-      this.fetchData();
+      // this.fetchData();
   }
   navigate(routeName,data){
     this.props.navigator.push({
@@ -68,7 +68,7 @@ export default class Public extends Component{
     })
   }
   componentDidMount(){
-    // this.fetchData();
+    this.fetchData();
   }
   _onRefresh() {
     this.setState({refreshing: true});
@@ -163,7 +163,7 @@ export default class Public extends Component{
       });
     },2000)
   }
-  fetchData() {
+  async fetchData() {
     let formdata = new FormData();
     formdata.append('id_user',this.state.user.id_user);
 
